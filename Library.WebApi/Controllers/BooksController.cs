0abe8e2d4abe9a626c554f5b0651b475c1cd2869
mkristic <marija.kristic@gmail.com>
2026-06-02 14:37:15 +0200

    files restored

---
+++ Library.WebApi/Controllers/BooksController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Library.WebApi.Models;
+
+namespace Library.WebApi.Controllers
+{
+    [ApiController]
+    [Route("books")]
+    public class BooksController : ControllerBase
+    {
+        private static List<Book> books = new()
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Dvorac",
+                Author = "Franz Kafka",
+                Year = 1926
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "Slika Doriana Graya",
+                Author = "Oscar Wilde",
+                Year = 1890
+            }
+        };
+
+        [HttpGet("GetBooks")]
+        public IActionResult Get()
+        {
+            return Ok(books);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetBook(int id)
+        {
+            var book = books.FirstOrDefault(book => book.Id == id);
+
+            if (book == null)
+            { 
+                return NotFound("Book not found.");
+            }
+
+            return Ok(book);
+        }
+
+        [HttpPost("PostBook")]
+        public IActionResult Post([FromBody]Book book)
+        {
+            book.Id = books.Max(book => book.Id) + 1;
+            books.Add(book);
+
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id}, book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Book updatedBook)
+        {
+            var book = books.FirstOrDefault(book => book.Id == id);
+
+            if (book == null)
+            {
+                return NotFound("Book not found.");
+            }
+
+            book.Title = updatedBook.Title;
+            book.Author = updatedBook.Author;
+            book.Year = updatedBook.Year;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var book = books.FirstOrDefault(book => book.Id == id);
+
+            if (book == null)
+            {
+                return NotFound("Book not found.");
+            }
+
+            books.Remove(book);
+
+            return NoContent();
+        }
+
+    }
+}
